677cbe95665e3374d381a441e816e876dcea5e54
Erik Peterson <elctrcpineapple@gmail.com>
2014-07-21 18:13:43 -0500

    migrate the roles

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140618023351) do
+ActiveRecord::Schema.define(version: 20140715015233) do
 
   create_table "events", force: true do |t|
     t.string   "name"
@@ -85,6 +85,11 @@ ActiveRecord::Schema.define(version: 20140618023351) do
     t.string   "firstname"
     t.string   "lastname"
     t.string   "phone"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "roles", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
   end
