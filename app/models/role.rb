0e117a993c908198f6ec2b92b74d264f5767ab1d
John Julien <john@julienfamily.com>
2014-08-06 21:37:13 -0500

    Annotate models

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,2 +1,11 @@
+# == Schema Information
+#
+# Table name: roles
+#
+#  id         :integer          not null, primary key
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 class Role < ActiveRecord::Base
 end
