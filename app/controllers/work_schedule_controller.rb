de8ad7d8448453387ed2641507c7c373bdf8e6a4
Clete Blackwell II <clete2@gmail.com>
2015-03-09 20:27:59 -0500

    Fixing delete button

--- app/controllers/work_schedule_controller.rb
+++ app/controllers/work_schedule_controller.rb
@@ -8,22 +8,18 @@ class WorkScheduleController < ApplicationController
 
   def new
     @work_schedule = WorkSchedule.new
-    @work_schedule.start_at=Time.now.change(hour: 8, min: 0)
-    @work_schedule.end_at=Time.now.change(hour: 16, min: 0)
+    @work_schedule.start_at = Time.now.change(hour: 8, min: 0)
+    @work_schedule.end_at = Time.now.change(hour: 16, min: 0)
 
     @people = Person.all
   end
 
   def update
-    @edit_work_schedule = WorkSchedule.find(params[:id])
-    @edit_work_schedule.staff_id = params[:person_id]
-    @zone = ' '+ DateTime.strptime(params[:work_schedule][:start_at][0..9], @@date_format).end_of_day.in_time_zone.zone
-    @edit_work_schedule.start_at = DateTime.strptime(params[:work_schedule][:start_at] + @zone, @@datetime_format)
-    @edit_work_schedule.end_at = DateTime.strptime(params[:work_schedule][:end_at] + @zone, @@datetime_format)
-    @edit_work_schedule.note = params[:work_schedule][:note]
+    @work_schedule = WorkSchedule.find(params[:id])
+    populate_work_schedule()
 
     respond_to do |format|
-      if @edit_work_schedule.save
+      if @work_schedule.save
         format.html { redirect_to action: 'index' }
       else
         format.html { redirect_to action: 'new' }
@@ -45,17 +41,14 @@ class WorkScheduleController < ApplicationController
   end
 
   def destroy
-    @work_schedule=WorkSchedule.all
+    @work_schedule = WorkSchedule.all
     @work_schedule.destroy(params[:id])
     redirect_to action: 'index'
   end
 
   def create
-    @work_schedule=WorkSchedule.new()
-    @work_schedule.note = params[:work_schedule][:note]
-    @zone = ' '+ DateTime.strptime(params[:work_schedule][:start_at][0..9], @@date_format).end_of_day.in_time_zone.zone
-    @work_schedule.start_at = DateTime.strptime(params[:work_schedule][:start_at] + @zone, @@datetime_format)
-    @work_schedule.end_at = DateTime.strptime(params[:work_schedule][:end_at] + @zone, @@datetime_format)
+    @work_schedule = WorkSchedule.new()
+    populate_work_schedule()
 
     if params[:person_id] != '' and Person.find(params[:person_id])
       @work_schedule.staff_id = params[:person_id]
@@ -71,5 +64,16 @@ class WorkScheduleController < ApplicationController
       flash[:alert] = 'Please enter valid staff name'
       render action: 'new'
     end
+  end
+
+  private
+  def populate_work_schedule
+    @work_schedule.note = params[:work_schedule][:note]
+    # Grab the zone from the user's input so that we can append it to the date.
+    # We take the end of the day so that if DST status changed that day it will pick up the new timezone offset.
+    @zone = ' '+ DateTime.strptime(params[:work_schedule][:start_at][0..9], @@date_format).end_of_day.in_time_zone.zone
+    # Now append it to each of the dates.
+    @work_schedule.start_at = DateTime.strptime(params[:work_schedule][:start_at] + @zone, @@datetime_format)
+    @work_schedule.end_at = DateTime.strptime(params[:work_schedule][:end_at] + @zone, @@datetime_format)
   end
 end
