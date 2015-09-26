863b4bb33dae105fd305f0ed7938aa29f3d29afe
Clete Blackwell II <clete2@gmail.com>
2015-09-26 13:59:39 -0500

    Add link in menu bar and add seed for local resource categories

---
+++ app/policies/local_resource_policy.rb
@@ -0,0 +1,39 @@
+class LocalResourceCategoryPolicy < ApplicationPolicy
+  @@admin_role = Role.find_by({name: 'Admin'})
+
+  def index?
+    @user.has_access?(PERM_RO_USER)
+  end
+
+  def show?
+    @user.has_access?(PERM_RO_USER)
+  end
+
+  def new?
+    @user.has_access?(PERM_ADMIN)
+  end
+
+  def edit?
+    @user.has_access?(PERM_ADMIN)
+  end
+
+  def bulk_assign_create?
+    @user.has_access?(PERM_ADMIN)
+  end
+
+  def bulk_assign_new?
+    @user.has_access?(PERM_ADMIN)
+  end
+
+  def create?
+    @user.has_access?(PERM_ADMIN)
+  end
+
+  def update?
+    @user.has_access?(PERM_ADMIN)
+  end
+
+  def destroy?
+    @user.has_access?(PERM_ADMIN)
+  end
+end
