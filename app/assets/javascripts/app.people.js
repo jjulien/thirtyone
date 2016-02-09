a4324d4220ff9e0ece71bc13f7a6c45066a30b26
David Leach <dleach@eastviewcc.org>
2016-02-08 20:05:56 -0600

    Namespace javascript files.

---
+++ app/assets/javascripts/app.people.js
@@ -0,0 +1,90 @@
+// app/assets/javascripts/app.people.js
+
+App.People = {};
+
+App.People.updateNewPersonButton = function(input) {
+  var value = input.value;
+  var add_button = $('#newperson');
+  if (value != "") {
+    add_button.removeClass('hide');
+    add_button.addClass('button');
+    add_button.val('+ Add ' + value);
+  } else {
+    add_button.addClass('hide');
+    add_button.removeClass('button');
+    add_button.html('');
+  }
+};
+
+App.People.updatePersonSearch = function(input, users_only, show_new_button) {
+  users_only = typeof users_only !== 'undefined';
+  show_new_button = typeof show_new_button !== 'undefined';
+
+  if ( show_new_button ) {
+    updateNewPersonButton(input);
+  }
+
+  var data = {ajax: true};
+
+  if (input.value.length > 0) {
+    var search_key_array = input.value.split(" ");
+    var search_keys = JSON.stringify(search_key_array);
+    data['search'] = search_keys;
+  }
+
+  if ( users_only ) { data['users_only'] = 'true'; }
+
+  var request = $.ajax({
+    url: "/people/search",
+    type: "GET",
+    data: data,
+    dataType: "html"
+  });
+
+  request.done(function (html) {
+    $("#results").html(html)
+  });
+
+  request.fail(function (jqXHR, textStatus) {
+    alert("Search update failed: " + textStatus);
+  });
+};
+
+App.People.editPerson = function(button) {
+  var li = $(button).closest("li");
+  var editting_div = $(li).find(".editting_div");
+  editting_div.show();
+  var edit_button = $(li).find(".edit_button");
+  edit_button.hide();
+  var raw1 = $(li).find(".raw1");
+  raw1.hide();
+  var raw2 = $(li).find(".raw2");
+  raw2.hide();
+  var edit_fields1 = $(li).find(".edit_fields1");
+  edit_fields1.show();
+  var edit_fields2 = $(li).find(".edit_fields2");
+  edit_fields2.show();
+};
+
+App.People.cancelEdit = function(button) {
+  var li = $(button).closest("li");
+  var editting_div = $(li).find(".editting_div");
+  editting_div.hide();
+  var edit_button = $(li).find(".edit_button");
+  edit_button.show();
+  var raw1 = $(li).find(".raw1");
+  raw1.show();
+  var raw2 = $(li).find(".raw2");
+  raw2.show();
+  var edit_fields1 = $(li).find(".edit_fields1");
+  edit_fields1.hide();
+  var edit_fields2 = $(li).find(".edit_fields2");
+  edit_fields2.hide();
+};
+
+$(document).ready(function() {
+  if (!($("body.people").length > 0)) {
+    return;
+  }
+  // Anything here will get executed when a page is rendered through the People controller
+});
